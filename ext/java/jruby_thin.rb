d0d81614f26c082f5889a916aeb9da9c54054fc0
Aman Gupta <aman@tmm1.net>
2010-10-13 02:18:39 -0700

    Port Thin's rack-compat parser changes to the jruby parser
    See http://gist.github.com/623663 for a list of changes in Thin's
    thin.c compared to Mongrel's original http11.c

--- ext/java/jruby_thin.rb
+++ ext/java/jruby_thin.rb
@@ -1,6 +1,33 @@
 require 'http11'
 
 module Thin
-  include Mongrel
-  InvalidRequest = HttpParserError
+  class HttpParser < Mongrel::HttpParser
+    def execute(env, data, nparsed)
+      new_nparsed = super
+
+      if body = env.instance_variable_get(:@http_body)
+        env.instance_variable_set(:@http_body, nil)
+
+        # delete unused entries
+        env.delete 'HTTP_CONTENT_LENGTH'
+        env.delete 'HTTP_CONTENT_TYPE'
+
+        # set defaults
+        env['SERVER_SOFTWARE'] = Thin::SERVER
+        env['PATH_INFO'] = env['REQUEST_PATH'] || ''
+        env['QUERY_STRING'] ||= ''
+        env['SCRIPT_NAME'] = ''
+        env['rack.url_scheme'] ||= 'http'
+
+        # convert to rack.input
+        if body.size > 0
+          env['rack.input'].write(body)
+        end
+      end
+
+      new_nparsed
+    end
+  end
+
+  InvalidRequest = Mongrel::HttpParserError
 end
